2753d87ed623e2fe909f73fade3ff719325a2002
Weronika Nowak <weronika.nowak@pollub.edu.pl>
2023-12-12 05:51:10 -0800

    Initial Commit

---
+++ kosciany_poker/kosciany_pokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  kosciany_pokerApp.swift
+//  kosciany_poker
+//
+//  Created by student on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct kosciany_pokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
